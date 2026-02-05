441df091d8a045022a535770f86ce97b46438062
Liang Geng <pwrliang@gmail.com>
2026-02-05 18:40:53 +0000

    Upload SF10

--- paper/02_run_benchmark.sh
+++ paper/02_run_benchmark.sh
@@ -123,8 +123,8 @@ for engine in "${ENGINES[@]}"; do
         --timeout "$QUERY_TIMEOUT" \
         --runs "$BENCHMARK_RUNS" \
         --scale-factor "$SCALE_FACTOR" \
-        --output "${RESULTS_DIR}/${engine}_results.json"
-
+        --output "${RESULTS_DIR}/${engine}_results.json" \
+        --queries "q2,q4,q6,q9,q10,q11"
     echo "Finished $engine"
 done
 
